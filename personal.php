a0f9b4141b543b6d2fe134777d013a2cbbd993b5
Vito <vito.pramudita1502@gmail.com>
2022-04-03 20:49:26 +0700

    Tried to change the file validation method

--- personal.php
+++ personal.php
@@ -1,10 +1,5 @@
-
-
-
-
 <!DOCTYPE html>
 <html lang="en">
-
 <head>
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
@@ -13,6 +8,7 @@
     <link rel="stylesheet" href="resources/css/responsive.css">
     <title>Resume Parser</title>
 </head>
+
 <body>
     <header> 
         <div class="logo">
